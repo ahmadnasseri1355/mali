216d7f3c8a88400e70ef46705b9d603ab21dfa00
sena123 <nasseri1355@gmail.com>
2024-01-25 09:45:29 +0330

    v۲

--- web/models.py
+++ web/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Expense(models.Model):
+	text = models.CharField(max_length=255)
+	date = models.DateTimeField(default=timezone.now)
+	amount = models.BigIntegerField(default=0)
+	user = models.ForeignKey(User,default=None,null=True,on_delete=models.SET_NULL,related_name="expenses",verbose_name="خریدار")
+
+class Incomes(models.Model):
+	text = models.CharField(max_length=255)
+	date = models.DateTimeField(default=timezone.now)
+	amount = models.BigIntegerField(default=0)
+	user = models.ForeignKey(User,default=None,null=True,on_delete=models.SET_NULL,related_name="incomes")
