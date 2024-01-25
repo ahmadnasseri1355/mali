435f3e2bf59ca36963697f30d14bebed274562e9
sena123 <nasseri1355@gmail.com>
2024-01-25 11:32:00 +0330

    v3

--- web/admin.py
+++ web/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 from .models import Expense,Incomes
 # Register your models here.
-admin.site.register(Expense)
+# admin.site.register(Expense)
 admin.site.register(Incomes)
+
+
+# Register your models here.
+admin.site.register(Expense)
+# @admin.register(Expense)
+class ExpenseAdmin(admin.ModelAdmin):
+    list_display = ('text','date','amount', 'user')
+    list_filter = (['text'])
+    search_fields = ('text','user')
